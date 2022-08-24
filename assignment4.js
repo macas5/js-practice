167ddb342cc21324c8450340cb1b8816c1c143e9
macas5 <mkalinauskas1993@gmail.com>
2022-08-24 17:35:12 +0300

    Add assignment description

--- assignment4.js
+++ assignment4.js
@@ -1,3 +1,6 @@
+// 4. Write a function that takes an array (a) and a number (n) as arguments 
+// /It should return the last n elements of a
+
 const lastElements = (array, number) => {
     const newArray = [];
     for (let i = (array.length - number); i < array.length; i++) {
